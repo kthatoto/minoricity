a5b0bb6b8e6e42e386d4027d4720ec348eb4dac7
kthatoto <kthatoto@gmail.com>
2018-07-20 22:07:09 +0900

    Added header in top

---
+++ src/components/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="index">
+    <h1>Minoricity</h1>
+    <h2>Coming Soon...</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style scoped>
+.index {
+  text-align: center;
+  padding-top: 200px;
+}
+</style>
